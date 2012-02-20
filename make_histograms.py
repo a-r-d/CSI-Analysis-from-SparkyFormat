78cab2c1b8720fec340e4eb6e4a534f1b97e8a00
Aaron Decker <d00blea.ard@gmail.com>
2012-02-20 13:00:01 -0500

    added a grapher for the diffs in shifts

--- make_histograms.py
+++ make_histograms.py
@@ -21,7 +21,9 @@ def read_line_position(lines, pos):
 	for line in lines:
 		splits = line.split(' ')
 		print splits[pos]
-		histList.append(int(splits[pos]))
+		# in case we have endline chars:
+		splits[pos] = splits[pos].replace('\n', '')
+		histList.append(float(splits[pos]))
 	return histList
 
 
@@ -60,13 +62,50 @@ def make_plot(histList, f1):
 	plt.text(0.75, (len_list * 0.9), freq_neg1)
 	
 	plt.show()
+	
 
+def get_diffs(list1, list2):
+	# List 1 = experimental
+	# list 2 = index
+	
+	diffs =[]
+	i = 0
+	for x in list1:
+		diff = float(x) - float(list2[i])
+		diffs.append(diff)
+		i += 1
+	
+	print diffs
+	return diffs
+	
+	
+	
+	
+def make_plot_diffs(histList, f1):
+	#nothing yet
+	len_list = len(histList)
+	plt.hist(histList, facecolor='g')
+	plt.ylabel('Number of occurences')
+	plt.xlabel("Diff btwn Shifts, for file %s" % f1)
+	plt.axis([-5, 5, 0, len_list])
+	plt.show()
+	
+	
 def load_graph(filename):
 	lines = import_results(filename)
 	histList = read_line_position(lines, 2)
 	make_plot(histList, filename)
-
-
+	
+	# load for diffs:
+	#lines = import_results(filename)
+	#histList1 = read_line_position(lines, 3)
+	#histList2 = read_line_position(lines, 4)
+	#histList3 = get_diffs(histList1, histList2)
+	#make_plot_diffs(histList3, filename)
+	
+	
+	
+	
 def main():
 	f1 = "results/ca.txt"
 	f2 = "results/cb.txt"
